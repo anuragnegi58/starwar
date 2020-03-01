72757ed0e75ab00fb5ff81377e2d7e86c5138874
Anurag Negi <anurag.negi@soprasteria.com>
2020-03-01 13:45:49 +0530

    few more changes.

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,22 +1,26 @@
-﻿﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+﻿﻿import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthenticationService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-public currentUserSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-public authenticateUser(username, password) {
-return this.http.get('https://swapi.co/api/people/');
-}
+  public currentUserSubject: BehaviorSubject<any> = new BehaviorSubject<any>(
+    null
+  );
+  public authenticateUser(username, password) {
+    return this.http.get(`https://swapi.co/api/people/`);
+  }
 
-public getData() {
-    return this.http.get('https://swapi.co/api/people/');
-    }
+  public getData() {
+    return this.http.get(`https://swapi.co/api/people/`);
+  }
 
-public getPlanetData() {
-        return this.http.get('https://swapi.co/api/planets/');
-        }
+  public getPlanetData() {
+    return this.http.get(`https://swapi.co/api/planets/`);
+  }
+  public getPlanetDetails(id) {
+    return this.http.get(`https://swapi.co/api/planets/${id}`);
+  }
 }
--- src/app/planet-details/planet-details.component.ts
+++ src/app/planet-details/planet-details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../authentication.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-planet-details',
-  templateUrl: './planet-details.component.html',
-  styleUrls: ['./planet-details.component.css']
+  selector: "app-planet-details",
+  templateUrl: "./planet-details.component.html",
+  styleUrls: ["./planet-details.component.css"]
 })
 export class PlanetDetailsComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  public planetDetails;
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.authenticationService.getPlanetDetails(id).subscribe(data => {
+      this.planetDetails = data;
+    });
   }
-
+  
+  public goBack() {
+    this.router.navigate(["dashboard"]);
+  }
 }
