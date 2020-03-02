eec6f853a54e8e918471f27697549057f4926e95
Anurag Negi <anurag.negi@soprasteria.com>
2020-03-02 19:31:07 +0530

    unit test

--- src/app/loggeduserguard.guard.spec.ts
+++ src/app/loggeduserguard.guard.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Router } from "@angular/router";
 import { LoggeduserguardGuard } from './loggeduserguard.guard';
 
 describe('LoggeduserguardGuard', () => {
   let guard: LoggeduserguardGuard;
-
+  let router = {
+    navigate: jasmine.createSpy(''),    // to spy on the url that has been routed
+};
+beforeEach((() => {
+  TestBed.configureTestingModule({
+    
+    providers: [
+      {provide: Router, useValue: router},
+  
+    ]
+  }).compileComponents();
+}));
   beforeEach(() => {
     TestBed.configureTestingModule({});
     guard = TestBed.inject(LoggeduserguardGuard);
