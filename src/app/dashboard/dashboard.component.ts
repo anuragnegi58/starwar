cca615d6dcc074a25ace8d1dc893b7f727ce9650
Anurag Negi <anurag.negi@soprasteria.com>
2020-03-02 15:10:58 +0530

    ui changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit, ElementRef, ViewChild,
-  AfterViewInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  AfterViewInit
+} from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { AuthenticationService } from "../authentication.service";
 import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-dashboard",
@@ -14,6 +20,10 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   public suggestions;
   public suggestedPlanets = [];
   public username: string;
+  public subject = new Subject<string>();
+  public setTimer;
+  public count = 0;
+  public isSearchDisabled: boolean;
   typeahead: FormControl = new FormControl();
   constructor(
     private planetDetails: AuthenticationService,
@@ -21,26 +31,49 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit(): void {
-    this.username = localStorage.getItem('user');
+    this.username = localStorage.getItem("user");
+    if (this.username !== "Luke Skywalker") {
+    this.setTimer = setInterval(() => {
+      this.count = 0;
+      this.typeahead.enable();
+      this.isSearchDisabled = false;
+    }, 60000);
+  }
     this.planetDetails.getPlanetData().subscribe(data => {
       this.suggestions = data;
     });
   }
 
   public suggest() {
-    if (!this.typeahead.value || this.typeahead.value === '') {
+    if (!this.typeahead.value || this.typeahead.value === "") {
       this.suggestedPlanets = [];
     } else {
       if (this.suggestions && this.suggestions.results) {
         this.suggestedPlanets = this.suggestions.results
-        .filter(data => data.name.toLowerCase().includes(this.typeahead.value.toLowerCase()))
-        .slice(0, 5);
+          .filter(data =>
+            data.name.toLowerCase().includes(this.typeahead.value.toLowerCase())
+          )
+          .slice(0, 5);
       }
     }
   }
+  public restrictSearch() {
+    if (this.username !== "Luke Skywalker") {
+      this.count++;
+      if (this.count <= 15) {
+        this.suggest();
+      } else {
+        this.suggestedPlanets = [];
+        this.typeahead.disable();
+        this.isSearchDisabled = true;
+      }
+    } else {
+      this.suggest();
+    }
+  }
   public logout() {
-    localStorage.removeItem('user');
-    this.router.navigate(['']);
+    localStorage.removeItem("user");
+    this.router.navigate([""]);
   }
 
   public ngAfterViewInit() {
